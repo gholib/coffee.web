e9525bcd5fd4b0417b2cfd91a86281b96337de58
gholib <gtrandnet@gmail.com>
2020-11-14 12:23:37 +0500

    f

--- src/layouts/components/navbar/components/ProfileDropDown.vue
+++ src/layouts/components/navbar/components/ProfileDropDown.vue
@@ -2,14 +2,14 @@
   <div class="the-navbar__user-meta flex items-center" v-if="activeUserInfo.displayName">
 
     <div class="text-right leading-tight hidden sm:block">
-      <p class="font-semibold">Админ</p>
-      <small>creativist</small>
+      <p class="font-semibold">COFFEE</p>
+      <!-- <small>creativist</small> -->
     </div>
 
     <vs-dropdown vs-custom-content vs-trigger-click class="cursor-pointer">
 
       <div class="con-img ml-3">
-        <img v-if="activeUserInfo.photoURL" key="onlineImg" :src="activeUserInfo.photoURL" alt="user-img" width="40" height="40" class="rounded-full shadow-md cursor-pointer block" />
+        <img v-if="activeUserInfo.photoURL" key="onlineImg" src="https://apicreativity.site/api/images/avatar-default.jpg" alt="user-img" width="40" height="40" class="rounded-full shadow-md cursor-pointer block" />
       </div>
 
       <vs-dropdown-menu class="vx-navbar-dropdown">
