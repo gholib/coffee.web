62f4a1f8e8e2c709dae20b316ef0d436ba2a259f
gholib <gtrandnet@gmail.com>
2020-11-18 14:33:42 +0500

    fix

--- src/views/calculation/Leftover.vue
+++ src/views/calculation/Leftover.vue
@@ -6,10 +6,10 @@
           </div>
           <vs-divider class="my-6"></vs-divider>
           <div class="vx-row">
-            <div v-for="(calc, index) in calculations" :key="index" class="vx-col w-1/2 mb-3">
+            <div v-for="(calc, index) in imports" :key="index" class="vx-col w-1/2 mb-3">
               <p><span class="">
                   {{calc.import_display_name}}
-                </span>, остаток {{calc.import_quantity - calc.count}}</p>
+                </span>, остаток {{calc.import_quantity - getSalesCount(calc.import_name)}}</p>
                 <p v-if="calc.import_quantity - calc.count < 0">Ошибка, что то ввели неправильно, свяжитесь с Голибом, +992880806776</p>
               <vs-progress class="block mt-1" :percent="(calc.import_quantity - calc.count)/calc.import_quantity * 100" :color="colors[Math.floor(Math.random() * 5)]"></vs-progress>
             </div>
@@ -24,7 +24,8 @@
 export default {
     data() {
       return {
-        calculations:[],
+        sales:[],
+        imports:[],
         colors:[
             'primary',
             'success',
@@ -41,8 +42,12 @@ export default {
     methods:{
       getCalculation(){
         this.$http.get('leftovers').then(response => {
-          this.calculations = response.data.calculations
+          this.sales = response.data.sales
+          this.imports = response.data.imports
         })
+      },
+      getSalesCount(importName){
+        return this.sales.filter(el => el.import_name === importName)[0].count
       }
     },
 
